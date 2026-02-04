9cf5ca050a9cc8e151cc0e619749f1da52098ffa
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-02-04 16:57:25 +0800

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "I use vim btw!"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+})
