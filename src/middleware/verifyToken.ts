1d9963cd70442f253d8dd8b102a9596bb058d285
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-02-06 21:38:09 +0800

    add verifyToken middleware

---
+++ src/middleware/verifyToken.ts
@@ -0,0 +1,23 @@
+import type {RequestHandler} from "express";
+import jwt from "jsonwebtoken";
+
+export const verifyToken: RequestHandler = async (req, res, next) => {
+    // Authorization: Bearer <token>
+    const bearerHeader = req.headers['authorization']
+
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+
+        const bearerToken = bearer[1];
+
+        try {
+            const decoded = jwt.verify(bearerToken, process.env.JWT_SECRET as string) as JwtPayload
+            req.user = { id: decoded.sub };
+            next()
+        } catch(err) {
+            res.sendStatus(401)
+        }
+    } else {
+        res.sendStatus(403);
+    }
+}
