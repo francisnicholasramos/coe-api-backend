f51c1d91a74a32a019c3f9bc49c545c64fc5b487
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-02-10 20:02:50 +0800

    setup routes, json/urlencoded parsers, and error handling

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,35 @@
 import express from "express";
+import {Request, Response, NextFunction} from "express";
+
+import auth from "./auth/auth.router";
+import postRoutes from "./posts/post.router";
+import commentRoutes from "./comments/comment.router"
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "I use vim btw!"
+// parsers
+app.use(express.urlencoded({ extended: true, limit: '5mb'}));
+app.use(express.json({limit: '5mb'}));
+
+// base routes
+app.use(auth)
+app.use("/", postRoutes)
+app.use("/", commentRoutes)
+
+// error handling
+app.use((
+    err: any, 
+    req: Request, 
+    res: Response, 
+    next: NextFunction
+) => {
+    console.log(err);
+    res.status(500).json({
+        message: process.env.NODE_ENV === 'production'
+            ? "Internal Server Error"
+            : err.message
     })
 })
 
