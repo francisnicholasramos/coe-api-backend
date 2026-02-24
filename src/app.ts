6090c7486416777a7da3ce15db4fba9c30855e8b
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-02-24 20:45:34 +0800

    scoped origins for production

--- src/app.ts
+++ src/app.ts
@@ -25,9 +25,8 @@ app.use(cookieParser());
 // cors 
 app.use(cors({
     origin: [
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "http://localhost:3001",
+        process.env.MAIN_APP || "http://localhost:5173",
+        process.env.API_URL || "http://localhost:3000",
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
