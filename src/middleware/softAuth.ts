7d9291e7bbf3cec6156be129315271c568e0269e
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-02-24 20:25:07 +0800

    refactor: change from bearer to httpOnly cookie and include necessary property in request

--- src/middleware/softAuth.ts
+++ src/middleware/softAuth.ts
@@ -2,21 +2,13 @@ import type {RequestHandler} from "express";
 import jwt from "jsonwebtoken";
 
 export const softAuth: RequestHandler = async (req, res, next) => {
-    const bearerHeader = req.headers['authorization']
+    const accessToken = req.cookies?.accessToken;
 
-    if (!bearerHeader) return next(); // proceed as anonymous comment
+    if (!accessToken) return next(); // proceed as anonymous comment
 
     try {
-        const bearer = bearerHeader.split(' ');
-
-        if (bearer.length !== 2 || bearer[0] !== 'Bearer') {
-            return next() // invalid format, continue as anonymous
-        }
-
-        const bearerToken = bearer[1];
-
-        const decoded = jwt.verify(bearerToken, process.env.JWT_SECRET as string) as JwtPayload
-        req.user = { id: decoded.id };
+        const decoded = jwt.verify(accessToken, process.env.JWT_SECRET as string) as JwtPayload
+        req.user = { id: decoded.id, username: decoded.username };
         next()
     } catch (err) {
         next() // token invalid/expired continue as anonymous
